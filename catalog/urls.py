2b9fc24da2e65a80080135217692377c7209e18f
Maksuda Islam Lima <maksudaislam@iut-dhaka.edu>
2021-04-23 19:40:17 +0600

    test files added

--- catalog/urls.py
+++ catalog/urls.py
@@ -15,7 +15,7 @@ urlpatterns = [
 
 urlpatterns += [
     path('mybooks/', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
-    path(r'borrowed/', views.LoanedBooksAllListView.as_view(),
+    path('borrowed/', views.LoanedBooksAllListView.as_view(),
          name='all-borrowed'),  # Added for challenge
 ]
 
