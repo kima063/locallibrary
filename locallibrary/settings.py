9c2641df2ad109741d05bd89f96fd9985ff853f7
Maksuda Islam Lima <maksudaislam@iut-dhaka.edu>
2021-04-26 17:52:37 +0600

    Update ALLOWED_HOSTS with site and development server URL

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 
-ALLOWED_HOSTS = ['limitless-ravine-76607.herokuapp.com']
+ALLOWED_HOSTS = ['limitless-ravine-76607.herokuapp.com','127.0.0.1']
 
 
 # Application definition
